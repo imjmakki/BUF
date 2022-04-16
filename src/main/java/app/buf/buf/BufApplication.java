d80fa9241e8b6fb1d0220c189b72a1064d59910a
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-16 11:38:48 +0300

    BUF

---
+++ src/main/java/app/buf/buf/BufApplication.java
@@ -0,0 +1,13 @@
+package app.buf.buf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BufApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/buf/buf/BufApplicationTests.java
@@ -0,0 +1,13 @@
+package app.buf.buf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BufApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
