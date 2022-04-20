8340fef2d3558d6ad236a4e7505699325380be0e
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-20 12:10:37 +0300

    BUF

--- src/main/java/app/buf/buf/dao/RoleDAO.java
+++ src/main/java/app/buf/buf/dao/RoleDAO.java
@@ -1,4 +1,12 @@
 package app.buf.buf.dao;
 
-public interface RoleDAO {
+import app.buf.buf.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleDAO extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String roleName);
 }
--- src/main/java/app/buf/buf/dao/UserDAO.java
+++ src/main/java/app/buf/buf/dao/UserDAO.java
@@ -1,4 +1,16 @@
 package app.buf.buf.dao;
 
-public interface UserDAO {
+import app.buf.buf.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDAO extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
