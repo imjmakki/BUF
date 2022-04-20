3c1a97e49b299014dd3eb94d4719684b5fd2988e
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-20 12:00:55 +0300

    BUF

--- src/main/java/app/buf/buf/model/User.java
+++ src/main/java/app/buf/buf/model/User.java
@@ -1,9 +1,19 @@
 package app.buf.buf.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.io.Serializable;
 @Entity
-@Table(name = "users")
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "phone"),
+                @UniqueConstraint(columnNames = "email")
+        })
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User implements Serializable {
 }
