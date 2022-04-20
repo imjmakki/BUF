f95f59e7da6a442c58535fb371165e5df6b6573d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-20 12:03:54 +0300

    BUF

--- src/main/java/app/buf/buf/model/User.java
+++ src/main/java/app/buf/buf/model/User.java
@@ -1,11 +1,13 @@
 package app.buf.buf.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
+import java.util.Date;
+
 @Entity
 @Table(name = "users",
         uniqueConstraints = {
@@ -16,4 +18,34 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Data
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Long id;
+
+    @Column(name = "uuid")
+    private String userId;
+
+    @Column(name = "phone")
+    @NotEmpty
+    private String username;
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotEmpty
+    private String password;
+    private boolean isActive;
+    private boolean isNotLocked;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date joinDate;
+
+    @Column(name = "last_login_date")
+    private Date lastLoginDate;
+
+    @Column(name = "last_login_date_display")
+    private Date lastLoginDateDisplay;
 }
