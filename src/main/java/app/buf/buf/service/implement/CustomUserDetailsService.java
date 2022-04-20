00d208db64dde0f7572cbcf4f1c33e7fbbf3e6c7
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-20 12:39:00 +0300

    BUF

--- src/main/java/app/buf/buf/service/implement/CustomUserDetailsService.java
+++ src/main/java/app/buf/buf/service/implement/CustomUserDetailsService.java
@@ -34,6 +34,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     private Collection< ? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles){
-        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toList());
     }
 }
