64391cec7ac0bcb34e25508beb99a2f33b3c6a2f
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-20 12:16:55 +0300

    BUF

--- src/main/java/app/buf/buf/service/implement/UserServiceImpl.java
+++ src/main/java/app/buf/buf/service/implement/UserServiceImpl.java
@@ -1,4 +1,10 @@
 package app.buf.buf.service.implement;
 
-public class UserServiceImpl {
+import app.buf.buf.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
 }
