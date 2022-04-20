0e6686960f8ee7ab3a16efd5bd2dd88cdc06ef5d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-20 12:34:32 +0300

    BUF

--- src/main/java/app/buf/buf/model/File.java
+++ src/main/java/app/buf/buf/model/File.java
@@ -1,4 +1,31 @@
 package app.buf.buf.model;
 
-public class File {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "files")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class File implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String id;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "file_type")
+    private String fileType;
+
+    @Lob
+    @Column(name = "data")
+    private byte[] data;
 }
