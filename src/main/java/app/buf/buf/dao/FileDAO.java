f5feccf25c9fdd796522c65c50ddbf77745476c8
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-20 13:49:42 +0300

    BUF

--- src/main/java/app/buf/buf/dao/FileDAO.java
+++ src/main/java/app/buf/buf/dao/FileDAO.java
@@ -1,4 +1,9 @@
 package app.buf.buf.dao;
 
-public interface FileDAO {
+import app.buf.buf.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileDAO extends JpaRepository<File, String> {
 }
--- src/main/java/app/buf/buf/dao/FileUpload.java
+++ src/main/java/app/buf/buf/dao/FileUpload.java
@@ -2,6 +2,8 @@ package app.buf.buf.dao;
 
 import app.buf.buf.model.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FileUpload extends JpaRepository<File, Long> {
+@Repository
+public interface FileUpload extends JpaRepository<File, String> {
 }
