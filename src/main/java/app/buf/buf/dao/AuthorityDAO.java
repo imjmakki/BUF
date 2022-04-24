6aa3412966946ee62338bb95cfb7cc37b0cd1ec2
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 17:27:37 +0300

    BUF

--- src/main/java/app/buf/buf/dao/AuthorityDAO.java
+++ src/main/java/app/buf/buf/dao/AuthorityDAO.java
@@ -1,4 +1,9 @@
 package app.buf.buf.dao;
 
-public interface AuthorityDAO {
+import app.buf.buf.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityDAO extends JpaRepository<Authority, Long> {
 }
